be0b547eadb2f5112c99679dea2428358fca44d7
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-10-16 22:14:49 +0300

    Test E is completed

---
+++ Eexpression-1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int x;
+    int result;
+    cin >> x;
+    result = 7 * x * x - 3 * x + 6;
+    cout << result << endl;
+    return 0;
+}
